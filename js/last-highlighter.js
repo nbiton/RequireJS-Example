83bb150a31c5c0427afbd69054cece7f00fdc472
Naor Biton <naor.biton@gmail.com>
2013-07-08 14:06:02 +0300

    Initial push with the base of the example

---
+++ js/last-highlighter.js
@@ -0,0 +1,25 @@
+/**
+ * Created with JetBrains WebStorm.
+ * User: Naor.Biton
+ * Date: 7/8/13
+ * Time: 1:55 PM
+ * To change this template use File | Settings | File Templates.
+ */
+(function(){
+    "use strict";
+
+    var $lastElement;
+
+    function highlight($element){
+        if ($lastElement){
+            $lastElement.css("background-color", "");
+        }
+
+        $lastElement = $element;
+
+        $element.css("background-color", "yellow");
+    }
+
+    // ?
+
+})();
